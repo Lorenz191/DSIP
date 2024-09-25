1e6b48617b97a81f925b8d4e5efda48ac6821c61
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2024-09-25 11:07:18 +0200

    fix: Delete dsip directory

--- dsip/commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
-    'subject-case': [2, 'always', 'sentence-case'],
-  },
-};
