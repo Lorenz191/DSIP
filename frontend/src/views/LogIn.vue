e7220ac31dc0f4ee2c7a3cb3ca57bf1c31270374
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2024-10-16 22:23:43 +0200

    feat: added BaseInput component

--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -8,27 +8,10 @@
 
     </div>
     <div class="login-container">
-
     </div>
   </div>
 </template>
 
 <style scoped>
-.container{
-  display: flex;
-  flex-direction: row;
-}
 
-.design-container, .login-container {
-  height: 100vh;
-}
-
-.design-container {
-  width: 30vw;
-  background: #0F8C3B;
-}
-
-.login-container {
-  width: 70vw;
-}
 </style>
