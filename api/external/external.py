b89ac9fe3327df19acb491882fb5bb95da641a88
philipp <1053@htl.rennweg.at>
2024-11-03 00:06:30 +0100

    DSIP API Struktur festgelegt

---
+++ api/external/external.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+def authenticate_with_external_service(request):
+    """Authentifiziert sich bei einem externen Dienst."""
+    pass
---
+++ api/forum/forum.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+def create_forum_post(request):
+    """Erstellt einen neuen Forum-Beitrag."""
+    pass
+
+def get_forum_posts(request):
+    """Gibt alle Forum-Beiträge zurück."""
+    pass
+
+def create_forum_comment(request, post_id):
+    """Erstellt einen neuen Kommentar zu einem Forum-Beitrag."""
+    pass
+
+def get_forum_comments(request, post_id):
+    """Gibt alle Kommentare zu einem Forum-Beitrag zurück."""
+    pass
