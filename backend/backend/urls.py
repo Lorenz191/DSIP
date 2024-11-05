348423a0972ac22392850f5284b46b75d6c3be2e
philipp <1053@htl.rennweg.at>
2024-11-05 16:20:19 +0100

    API_Urls weitermachen

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,10 @@ from .DSIP import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("sean/<str:text>/", views.sean_view),
-    path("api/admin/<str:post_id>/", views.view_delete_user_post),
+    path("api/admin/delete/<str:post_id>/", views.view_delete_user_post),
     path("api/admin/comment/<str:post_id>/", views.view_comment_as_admin),
+    path("api/external/", views.),
+    path("api/forum/getPosts", views.view_get_forum_posts),
+    path("api/forum/createComment/<str:post_id>", views.view_create_forum_comment),
+    path("api/forum/getComments/<str:post_id>", views.view_get_forum_comments),
 ]
