07dc821d8c48be01c3ee45d7ea24167f4a0bfa56
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2024-11-12 11:29:05 +0100

    feat: prepared css struct

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,10 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './index.css'
+import './assets/tailwind.css'
+import './style.css'
+import PrimeVue from 'primevue/config';
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: 'none'
+})
 
 app.mount('#app')
