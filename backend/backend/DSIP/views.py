043794aba026eb6530655edc992187dbf6d34b12
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2024-11-12 07:31:50 +0100

    fix: deleted unnecessary methods

--- backend/backend/DSIP/views.py
+++ backend/backend/DSIP/views.py
@@ -30,7 +30,9 @@ def sean_view(request, text):
     sentiment = SeAn().get_sentiment(text)
     return HttpResponse(sentiment[0])
 
-#ADMIN
+
+# ADMIN
+
 
 def view_delete_user_post(request, post_id):
     status = delete_user_post(post_id=post_id)
@@ -41,44 +43,17 @@ def view_comment_as_admin(request, post_id):
     status = comment_as_admin(post_id=post_id)
     return HttpResponse(status=status)
 
-#EXTERNAL
+
+# EXTERNAL
+
 
 def view_authenticate_with_external_service(request):
     """Authentifiziert sich bei einem externen Dienst."""
     pass
 
 
-#FORUM
+# POST
 
-def view_create_forum_post(request):
-    """Erstellt einen neuen Forum-Beitrag."""
-    pass
-
-
-def view_get_forum_posts(request):
-    """Gibt alle Forum-Beiträge zurück."""
-    pass
-
-
-def view_analyze_sentiment(request, post_id):
-    """Analysiert die Stimmung eines Beitrags."""
-    pass
-
-
-def view_moderate_post(request, post_id):
-    """Moderiert einen Beitrag und markiert unangemessene Inhalte."""
-    pass
-
-def view_create_forum_comment(request, post_id):
-    """Erstellt einen neuen Kommentar zu einem Forum-Beitrag."""
-    pass
-
-
-def view_get_forum_comments(request, post_id):
-    """Gibt alle Kommentare zu einem Forum-Beitrag zurück."""
-    pass
-
-#POST
 
 def view_create_post(request, post_body):
     """Erstellt einen neuen Vorschlag."""
@@ -109,23 +84,9 @@ def view_downvote_post(request, post_id):
     """Verringert die Bewertung eines Vorschlags."""
     pass
 
-#SORT
 
-def view_sort_posts_by_popularity(request):
-    """Sortiert die Vorschläge nach Beliebtheit."""
-    pass
+# USERS
 
-
-def view_sort_posts_by_newest(request):
-    """Sortiert die Vorschläge nach den neuesten zuerst."""
-    pass
-
-
-def view_sort_posts_by_oldest(request):
-    """Sortiert die Vorschläge nach den ältesten zuerst."""
-    pass
-
-#USERS
 
 def view_register_user(request):
     """Registriert einen neuen Benutzer."""
@@ -142,6 +103,6 @@ def view_get_user_profile(request, user_id):
     pass
 
 
-def view_update_user_profile(request, user_id):
-    """Aktualisiert das Benutzerprofil."""
+def view_delete_user_profile(request, user_id):
+    """Löscht das Benutzerprofil."""
     pass
