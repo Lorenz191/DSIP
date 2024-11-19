33ecf73b5ddff819ea0e32d360ce68ce9e19fd37
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2024-11-19 17:18:10 +0100

    feat:started with vue components

--- frontend/src/views/LogInView.vue
+++ frontend/src/views/LogInView.vue
@@ -5,9 +5,25 @@
 <template>
   <div class="container">
     <div class="design-container">
-
+      <img src="../components/icons/Title_Picture.svg" alt="Picture">
     </div>
     <div class="login-container">
+      <div class="traditional-login">
+        <div class="username">
+          <label for="uname">Benutzername</label>
+          <input id="uname" name="uname" type="text">
+        </div>
+        <div class="password">
+          <label for="password">Passwort</label>
+          <input type="password" id="password" name="password">
+        </div>
+        <div class="login-button">
+          <button type="submit">Anmelden</button>
+        </div>
+      </div>
+      <div>
+
+      </div>
     </div>
   </div>
 </template>
