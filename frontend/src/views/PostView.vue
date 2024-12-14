22032ba2421546dc68422f66b43c280755cb920c
Lorenz191 <144610453+Lorenz191@users.noreply.github.com>
2024-12-14 16:14:49 +0100

    fix: fixed PostView.vue through adding loading logic

--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -4,16 +4,18 @@ import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import { useRoute } from 'vue-router'
 
-const post = ref( [])
+const post = ref(null)
+const loading = ref(true)
 const route = useRoute()
 
 const fetchPost = async () => {
   try {
-    const response = await axios.post('http://localhost:8000/api/post/get/', { "post_id": route.params.id })
-    console.log(response.data)
+    const response = await axios.post('http://localhost:8000/api/post/get/', { 'post_id': route.params.id })
     post.value = response.data
   } catch (error) {
     console.error('Error fetching posts:', error)
+  } finally {
+    loading.value = false
   }
 }
 
@@ -22,21 +24,32 @@ onMounted(() => {
 })
 </script>
 
-<template>
-  <LandingNav></LandingNav>
 
-  <div class="post-info-container">
-    <div class="date-container">
-      <p class="date">Veröffentlicht am {{ new Date(post.created_at).toLocaleDateString() }}</p>
+<template>
+  <LandingNav :arrow="true"></LandingNav>
+
+  <div v-if="loading" class="loading-container">
+    <span class="loader"> </span>
+  </div>
+
+  <div v-else-if="post" class="post-info-container">
+    <div class="post-container">
+      <div class="date-container">
+        <p class="date">Veröffentlicht am {{ new Date(post.created_at).toLocaleDateString() }}</p>
+      </div>
+      <div class="title-container">
+        <h1 class="title">{{ post.body.title }}</h1>
+      </div>
+      <div class="text-container">
+        <p class="post-content">
+          {{ post.body.content }}
+        </p>
+      </div>
     </div>
-    <div class="title-container">
-      <h1 class="title">{{ post.body.title }}</h1>
-    </div>
-    <div class="text-container">
-      <p class="post-content">
-        {{post.body.content}}
-      </p>
-    </div>
+  </div>
+
+  <div v-else class="error-container">
+    <p>Error: Could not fetch post.</p>
   </div>
 </template>
 
@@ -59,7 +72,7 @@ onMounted(() => {
   line-height: normal;
 }
 
-.post-content{
+.post-content {
   color: #000;
   font-family: Futura;
   font-size: 16px;
@@ -67,4 +80,67 @@ onMounted(() => {
   font-weight: 500;
   line-height: normal;
 }
+
+.post-info-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 90px;
+}
+
+.post-container{
+  width: 50vw;
+}
+
+.title-container{
+  margin-bottom: 5px;
+}
+
+.loading-container {
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  width: 84px;
+  height: 84px;
+  position: relative;
+  overflow: hidden;
+}
+
+.loader:before, .loader:after {
+  content: "";
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  background: #2EDB7B;
+  transform: translate(-50%, 100%) scale(0);
+  animation: push 2s infinite ease-in;
+}
+
+.loader:after {
+  animation-delay: 1s;
+}
+
+@keyframes push {
+  0% {
+    transform: translate(-50%, 100%) scale(1);
+  }
+  15%, 25% {
+    transform: translate(-50%, 50%) scale(1);
+  }
+  50%, 75% {
+    transform: translate(-50%, -30%) scale(0.5);
+  }
+  80%, 100% {
+    transform: translate(-50%, -50%) scale(0);
+  }
+}
+
 </style>
